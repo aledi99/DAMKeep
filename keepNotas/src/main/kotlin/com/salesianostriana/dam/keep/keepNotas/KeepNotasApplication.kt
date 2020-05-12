95d98baab855febf90e426e7c68cc4ad5c8e7e8b
aledi99 <alediazuni@gmail.com>
2020-05-12 14:45:24 +0200

    Proyecto Spring inicializado

---
+++ keepNotas/src/main/kotlin/com/salesianostriana/dam/keep/keepNotas/KeepNotasApplication.kt
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.keep.keepNotas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeepNotasApplication
+
+fun main(args: Array<String>) {
+	runApplication<KeepNotasApplication>(*args)
+}
